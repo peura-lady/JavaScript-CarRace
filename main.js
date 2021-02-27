90183993dbacbac9e8e8bec953b1fd694b25ff71
Anna Sonninen <anna.sonninen94@gmail.com>
2021-02-27 19:21:19 +0200

    locked keys in start and stop run

--- main.js
+++ main.js
@@ -93,13 +93,19 @@ function playGame(){
 }
 
 function startRun(event){
-    event.preventDefault();
-    keys[event.key] = true;
+    
+    if(keys.hasOwnProperty(event.key)){
+        event.preventDefault();
+        keys[event.key] = true;
+    }
 }
 
 function stopRun(event){
-    event.preventDefault();
-    keys[event.key] = false;
+    
+    if(keys.hasOwnProperty(event.key)){
+        event.preventDefault();
+        keys[event.key] = false;
+    }
 }
 
 function moveRoad(){
