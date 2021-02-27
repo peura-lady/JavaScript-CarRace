a698645b5bb06bc5a125449d1f7334d1ec87b78c
Anna Sonninen <anna.sonninen94@gmail.com>
2021-02-27 15:41:37 +0200

    added crash setting and scores

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ function getQuantityElements(heightElement) {
 
 function startGame(){
     start.classList.add('hide');
+    gameArea.innerHTML = '';
+    
 
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
@@ -50,8 +52,12 @@ function startGame(){
         gameArea.appendChild(enemy);
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = '180px';
+    car.style.top = 'auto';
+    car.style.bottom = '15px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -59,6 +65,8 @@ function startGame(){
 
 function playGame(){
     if (setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE:<br>' + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
@@ -108,7 +116,22 @@ function moveRoad(){
 
 function moveEnemy(){
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if(carRect.top <= enemyRect.bottom &&
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top){
+                setting.start = false;
+                console.warn('crash');
+                start.classList.remove('hide');
+                // start.style.top = score.offsetHeight;
+        }
+
+        
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if(item.y >= document.documentElement.clientHeight){
